345b66956bb7b34d53e239104c3c184a3d621d71
Artur Hallmann <artur.hallmann@oraise.com>
2012-04-21 02:09:22 +0200

    upgrade to AngularJS 1.0.0rc6 runny-nose

--- Medienverwaltung.angular/test/e2e/scenarios.js
+++ Medienverwaltung.angular/test/e2e/scenarios.js
@@ -23,4 +23,17 @@ describe('my app', function() {
     });
 
   });
+
+    /*
+     it('should filter the phone list as user types into the search box', function() {
+     expect(repeater('.phones li').count()).toBe(3);
+
+     input('query').enter('nexus');
+     expect(repeater('.phones li').count()).toBe(1);
+
+     input('query').enter('motorola');
+     expect(repeater('.phones li').count()).toBe(2);
+     });
+     */
+
 });
