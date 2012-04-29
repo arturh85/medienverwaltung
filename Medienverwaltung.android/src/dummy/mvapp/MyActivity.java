6d6bd6671b36aac5a2a9515c585e9c75a036abb7
Artur Hallmann <artur.hallmann@oraise.com>
2012-04-29 17:11:53 +0200

    worked on android application - should post isbn numbers to cloud service

--- Medienverwaltung.android/src/dummy/mvapp/MyActivity.java
+++ Medienverwaltung.android/src/dummy/mvapp/MyActivity.java
@@ -12,21 +12,23 @@ import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.net.URI;
 
 public class MyActivity extends Activity
 {
     private static final String TAG = "MyActivity";
+    private static final String API_BASE_URL = "http://www.medienverwaltung.net"
 
     /** Called when the activity is first created. */
     @Override
@@ -89,32 +91,82 @@ public class MyActivity extends Activity
         Toast.makeText(context, s, duration).show();
     }
     
-    public void selfDestruct(View view) {
-        // Kabloey
-
+    public void scanBooks(View view) {
         Intent intent = new Intent("com.google.zxing.client.android.SCAN");
         intent.setPackage("com.google.zxing.client.android");
         //intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
         startActivityForResult(intent, 0);
-
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        Log.d(TAG, "onActivityResult");
+
         if (requestCode == 0) {
             if (resultCode == RESULT_OK) {
+                // Handle successful scan
+
                 String contents = intent.getStringExtra("SCAN_RESULT");
                 String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-                // Handle successful scan
+
+                Log.d(TAG, "result format: " + format);
+                Log.d(TAG, "result contents: " + contents);
                 Context context = getApplicationContext();
-                CharSequence text = "Hello toast!";
                 int duration = Toast.LENGTH_SHORT;
 
                 Toast.makeText(context, format, duration).show();
                 Toast.makeText(context, contents, duration).show();
+
+
             } else if (resultCode == RESULT_CANCELED) {
                 // Handle cancel
+
+                Log.d(TAG, "onActivityResult");
+            } else {
+                Log.d(TAG, "unknown result code: " + resultCode);
             }
+        } else {
+            Log.d(TAG, "unknown request code: " + requestCode);
         }
+    }
+
+    public void addBookById(String id, String idFormat) {
+
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        try {
+            String url = "/api/media";
+            String postData = "{isbn: \"\" + id + \"\", isbn_format: \"\" + idFormat + \"\"}";
+
+            Log.d(TAG, "PUT " + url + ": " + postData);
+
+            HttpPut request = new HttpPut(API_BASE_URL + url);
+            request.setEntity(new StringEntity(postData));
+
+            HttpResponse response = httpclient.execute(request);
+            StatusLine statusLine = response.getStatusLine();
+            
+            Log.d(TAG, "response code: " + statusLine.getStatusCode());
+
+            HttpEntity entity = response.getEntity();
+            String responseData = IOUtils.toString(entity.getContent());
+
+            Log.d(TAG, "response data: " + responseData);
+            
+            if(statusLine.getStatusCode() == 200) {
+
+            } else {
+
+            }
+            
+        } catch(IOException e) {
+            Log.d(TAG, "failed to add books", e);
+        } finally {
+            // When HttpClient instance is no longer needed,
+            // shut down the connection manager to ensure
+            // immediate deallocation of all system resources
+            httpclient.getConnectionManager().shutdown();
+        }
+
+
     }
 
 }
